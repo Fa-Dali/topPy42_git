b2d454b813c10e26ea8776e72ba8b86e1840e9cd
Сергей Фадеев <fadeev.music.studio@yandex.ru>
2025-04-21 21:57:21 +0300

    Клумба. Отработаны стратегии для ситуаций: 0001 ... | ... 10001 ... | ... 1000 А также: 111 | 000

--- src/home_work/flowers_main.py
+++ src/home_work/flowers_main.py
@@ -1,46 +1,63 @@
-
-
+# ЧЕРНОВИК:
 # i  - счетчик символов :     i +=  1
 # j0 - счетчик нулей    :    j0 +=  1
-# j1 - счетчик единиц   : const  = -1
-# jc - счетчик блоков   :     10...01
+# k - отсечение первого места на клумбе
+# if (max_0 < 0) = 0
+# if (j0 > max_0) = max_0=j0
 #  1  1  0  0  0  1  1
 # -1 -1 +1 +1 +1 -1 -1
 #    -1  0  1  2
+
 def calculat(flow_place):
-    result, max_0, i, j0, j1 = 0, 0, 0, 0, 0
+    '''
+    Поиск максимального места клумбы для посадки группы цветов.
+    Условие: края группы новых цветов не должны контактировать с растущими
+    цветами на клумбе
+    '''
+    max_0, i, j0, k = 0, 0, 0, 0
+    # Обработка, если строка начинается на 0 : 0001 ...
     for i in range(len(flow_place)):
-        # print(flow_place[i], end="")
-        # if flow_place[-1] == '1':
-        #     result += -1
+        if flow_place[i] == '0' and k == 0:
+            j0 += 1; k = 1
+        else: break
+
+    # Обработка, если строка имеет вид : ... 10001 ...
+    for i in range(len(flow_place)):
         if flow_place[i] == '1':
-            if j0 > max_0:     # фиксируем max
-                result = j0
-            j0 = -1            # счетчик едини
-            i += 1
-        elif flow_place[i] == '0':
-            j0 += 1
-            i += 1
+            j0 -= 2
+            if j0 > max_0:
+                max_0 = j0
+            j0 = 0; i += 1
+        else:
+            j0 += 1; i += 1
 
+    # Обработка, если строка заканчивается на 0 : ... 1000
+    if j0 > max_0:
+        max_0 = j0 - 1
 
+    return max_0
 
-        if result < 0:
-            result = 0
-        # break
-    if i == len(flow_place):
-        if flow_place[-1] == '1':
-            result -= 1
-
-    return result
-
+def is_valid_input(flow_place):
+    # Проверяем, чтобы ввод содержал только '0' и '1'
+    return all(char in '01' for char in flow_place)
 
 def us_inp():
     while True:
-        flow_place = input("Покажите клумбу:\n")
-        if flow_place == "": break
-        calculat(flow_place)
-        print(f"Требуется {calculat(flow_place)} шт. цветов.")
+        flow_place = input("Покажите клумбу символами 0 и 1\nнапример : "
+                           "010000010\n"
+                           "Выход из программы : Enter\n"
+                           "-------------------------------\n")
+        if flow_place == "":
+            break
+
+        if not is_valid_input(flow_place):
+            print(
+                "Ошибка: допускаются только символы '0' и '1'. Попробуйте снова.")
+            continue  # Идём на следующую итерацию, чтобы запросить ввод снова
+
+        result = calculat(flow_place)
+        print(f"Ответ:\n  Требуется {result} шт. цветов.\n"
+              f"-------------------------------")
 
 if __name__ == "__main__":
     us_inp()
-
